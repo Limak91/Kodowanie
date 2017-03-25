4e2c2e42561e72c22d9404600e655a0fe9945a32
Kamil Kubacki <kamcioo1991@o2.pl>
2017-03-25 15:44:26 +0100

    First Committt

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-    angular.module("Sda", []).controller("usersCtrl", function ($scope){
-         $scope.hello = "Hello";
-         $scope.look = function(){
-          return $scope.limit < $scope.users.length
-         };
-         $scope.more = function(){
-         $scope.limit = $scope.limit +10;
-         };
-         $scope.limit = 2;
-         $scope.users = [
-         {
-             username: "Paweł",
-             role: "admin"},
-                 {
-             username: "Dawid",
-             role: "menager"},
-                 {
-             username: "Wojtek",
-             role: "boss"},
-                 {
-             username: "Hela",
-             role: "user"}
-         ];
-         }),controller("TabsCtrl", function($scope)
-         $scope.current = 1;
-         $scope.changeTab = function(tab){
-         $scope.current = tab;};
-         $scope.isCurrent = function(tab){
-         return tab === $scope.current;}
-         });
+angular.module("Sda", []).controller("usersCtrl", function ($scope, $http) {
+   $scope.loading = true;
+   $http.get("https://jsonplaceholder.typicode.com/users").then(function (res){
+   $scope.users = res.data;
+   $scope.loading = false;
+    });
+
+    $scope.limit = 2;
+    $scope.more = function () {
+        $scope.limit = $scope.limit + 10;
+    };
+
+    $scope.shouldShowMoreButton = function () {
+        return $scope.users && $scope.limit < $scope.users.length
+    };
+
+    $scope.remove = function (index) {
+        $scope.users.splice(index, 1);
+    };
+
+    $scope.addUser = function () {
+          $scope.users.push({
+              username: $scope.newName,
+              role: $scope.newRole
+          });
+          $scope.newName = "";
+          $scope.newRole = "";
+    };
+
+}).controller("TabsCtrl", function ($scope) {
+    $scope.current = 1;
+
+    $scope.changeTab = function (tab) {
+        $scope.current = tab;
+    };
+
+    $scope.isCurrent = function (tab) {
+        return tab === $scope.current;
+    }
+
+});
